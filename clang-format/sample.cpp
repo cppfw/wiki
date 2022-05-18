1c81aadc1a2baa35762535c5dfb453cb70216e99
Ivan Gagis <igagis@gmail.com>
2022-05-18 22:44:01 +0300

    .clang-format

---
+++ clang-format/sample.cpp
@@ -0,0 +1,11 @@
+class sample_class_a1
+{};
+
+class sample_class_a2
+{};
+
+class sample_class_b : public sample_class_a1
+{};
+
+class sample_class_c : public sample_class_a1, public sample_class_a2
+{};
---
+++ clang-format/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# we want exit immediately if any command fails and we want error in piped commands to be preserved
+set -eo pipefail
+
+script_dir="$(dirname $0)/"
+
+cd $script_dir
+
+clang-format sample.cpp > out.cpp
+
+cmp sample.cpp out.cpp
+
+rm out.cpp
