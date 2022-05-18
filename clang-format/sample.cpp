c69c6306d1f008d751cc012fad226869a700fdfa
Ivan Gagis <igagis@gmail.com>
2022-05-19 00:05:26 +0300

    stuff

--- clang-format/sample.cpp
+++ clang-format/sample.cpp
@@ -16,33 +16,32 @@ class sample_class_a1
 	int a;
 
 public:
-	sample_class_a1(int a) :
-		a(a)
-	{}
+	sample_class_a1(int a) :a(a){}
 };
 
 class sample_class_a2
 {};
 
-class sample_class_b : public sample_class_a1
+class sample_class_b : public sample_class_a1, private sample_sssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
 {};
 
+// TODO: the comment after : fails to format inheritance list and the comment itself
 class sample_class_c ://some class comment
-	public sample_class_a1,
-	public sample_class_a2
+public sample_class_a1, public sample_class_a2
 {
 public:
 	int a;//some comment
 	int b;
 
-	sample_class_c(int a, int b) :
+	sample_class_c(int a, int b, int ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc) :
 		sample_class_a1(a),
 		a(a),
 		b(b)
 	{}
 
-    void func(int a, int b, int cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc)
+    void func(int a, int b, int cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc)
 	{
-		std::cout << "a = " << a << std::endl;
+		std::cout << "printing values of different variables passed in as arguments to this function" << std::endl << "the first one would be:" <<std::endl
+		<<"a = " << a << std::endl;
 	}
 };
