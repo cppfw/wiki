e32ae41f0a19f33d730d164d6a4b4657072e1400
Ivan Gagis <igagis@gmail.com>
2023-03-07 10:07:22 +0200

    update conan templates

--- templates/conan/test_package/example.cpp
+++ templates/conan/test_package/example.cpp
@@ -1,11 +1,7 @@
-#include <clargs/parser.hpp>
+#include <iostream>
 
 int main(int argc, const char** argv){
-	clargs::parser p;
-
-	bool help = false;
-
-	p.add("help", "show help information", [&help](){help = true;});
-
-	std::cout << p.description() << '\n';
+	// implement simple testing
+	
+	std::cout << "test" << std::endl;
 }
